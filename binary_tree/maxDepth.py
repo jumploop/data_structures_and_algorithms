45259a22b5e3c27ee9f35ce05868badbb9998f54
liming <827182486@qq.com>
2020-06-27 17:15:26 +0800

    104. 二叉树的最大深度

--- binary_tree/maxDepth.py
+++ binary_tree/maxDepth.py
@@ -8,9 +8,26 @@
 
 """
  二叉树的最大深度
+104. 二叉树的最大深度
+给定一个二叉树，找出其最大深度。
 
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
 基本思路就是递归，当前树的最大深度等于（1+max(左子树最大深度，右子树最大深度)）。代码如下：
 """
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -18,10 +35,22 @@ class TreeNode:
         self.right = None
 
 
-def maxDepth(root):
-    if not root:
-        return 0
-    return 1 + max(maxDepth(root.left), maxDepth(root.right))
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        # return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1) if root else 0
+
+    def maxDepth2(self, root: TreeNode) -> int:
+        # 思路：分治法
+        if not root:
+            return 0
+        left = self.maxDepth2(root.left)
+        right = self.maxDepth2(root.right)
+        if left > right:
+            return left + 1
+        return right + 1
 
 
 if __name__ == '__main__':
@@ -40,5 +69,5 @@ if __name__ == '__main__':
     c.left = f
     c.right = g
 
-    result = maxDepth(a)
+    result = Solution().maxDepth(a)
     print(result)
