301693d491a0e04537dbd6d780cf6c6cfc604c9f
liming <827182486@qq.com>
2020-07-09 10:22:32 +0800

    61. 搜索区间

--- binary_search/myPow.py
+++ binary_search/myPow.py
@@ -44,7 +44,24 @@ class Solution:
             x *= x
             n >>= 1
         return res
+
+        #  算法流程：
+        # 1.当 x = 0.0 时：直接返回 0.0 ，以避免后续 1 除以 0 操作报错。分析： 数字 0 的正数次幂恒为 0 ； 0 的 0 次幂和负数次幂没有意义，因此直接返回 0.0 即可。
+        # 2.初始化 res = 1 。
+        # 3.当 n < 0 时：把问题转化至 n≥0 的范围内，即执行 x = 1/x ，n = - n 。
+        # 4.循环计算：当 n = 0时跳出。
+        #   当 n&1=1 时：将当前 x 乘入 res （即 res *= x）。
+        #   执行 x = x^2 （即 x *= x）。
+        #   执行 n 右移一位（即 n >>= 1）。
+        # 5.返回 res。
+        #
+        # 作者：jyd
+        # 链接：https://leetcode-cn.com/problems/powx-n/solution/50-powx-n-kuai-su-mi-qing-xi-tu-jie-by-jyd/
+        # 来源：力扣（LeetCode）
+        # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
 if __name__ == '__main__':
-    x=2
-    n=3
-    print(Solution().myPow2(x,n))
+    x = 2
+    n = 3
+    print(Solution().myPow2(x, n))
---
+++ binary_search/searchRange.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/9 9:12
+# @Author  : 一叶知秋
+# @File    : searchRange.py
+# @Software: PyCharm
+"""
+61. 搜索区间
+中文English
+给定一个包含 n 个整数的排序数组，找出给定目标值 target 的起始和结束位置。
+
+如果目标值不在数组中，则返回[-1, -1]
+
+样例
+例1:
+
+输入:
+[]
+9
+输出:
+[-1,-1]
+
+例2:
+
+输入:
+[5, 7, 7, 8, 8, 10]
+8
+输出:
+[3, 4]
+挑战
+时间复杂度 O(log n)
+"""
+
+
+class Solution:
+    """
+    @param A: an integer sorted array
+    @param target: an integer to be inserted
+    @return: a list of length 2, [index1, index2]
+    """
+
+    def searchRange(self, A, target):
+        # write your code here
+        if not A:
+            return [-1, -1]
+        first_pos = self.get_first_pos(A, target)
+        cur = first_pos
+        while 0 <= cur < len(A) - 1 and A[cur + 1] == target:
+            cur += 1
+        return [first_pos, cur]
+
+    def get_first_pos(self, A, target):
+        start, end = 0, len(A) - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if A[mid] < target:
+                start = mid
+            else:
+                end = mid
+        if A[start] == target:
+            return start
+        if A[end] == target:
+            return end
+        return -1
+
+
+if __name__ == '__main__':
+    A = [5, 7, 7, 8, 8, 10]
+    target = 8
+    print(Solution().searchRange(A, target))
