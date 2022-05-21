1a1d5e2ea7d5fadb10a446a8192d72a76ec2a40d
liming <827182486@qq.com>
2022-05-22 00:10:37 +0800

    21. 合并两个有序链表

--- linked_list/mergeTwoLists.py
+++ linked_list/mergeTwoLists.py
@@ -21,6 +21,9 @@
 
 
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -62,3 +65,25 @@ class Solution:
         else:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
+
+    def mergeTwoLists3(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # 虚拟头结点
+        dummy = ListNode(-1)
+        p = dummy
+        p1, p2 = list1, list2
+        while p1 and p2:
+            # 比较 p1 和 p2 两个指针
+            # 将值较小的的节点接到 p 指针
+            if p1.val > p2.val:
+                p.next = p2
+                p2 = p2.next
+            else:
+                p.next = p1
+                p1 = p1.next
+            # p 指针不断前进
+            p = p.next
+        if p1:
+            p.next = p1
+        if p2:
+            p.next = p2
+        return dummy.next
